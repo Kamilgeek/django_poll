da6f29200bbb0dfe51b8e2123bab44eb1617e04e
Kamilgeek <kamil-magomedov-00@mail.ru>
2020-02-18 10:14:35 +0400

    did it

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import HttpResponse
 from django.template import loader
 from .models import Question
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return  HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return  HttpResponse(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'quesion': question})
 
 def results(request, question_id):
     response = (f"You're looking at the results of question {question_id}.")
